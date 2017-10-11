42330f4f71dd4d9b457668c6c098fbb4722f2e42
Derric Gilling <derricgilling@gmail.com>
2017-10-10 18:38:05 -0700

    Bump: version to 1.1.0

--- src/Configuration.php
+++ src/Configuration.php
@@ -16,7 +16,7 @@ class Configuration {
      * @var string
      */
     public static $BASEURI = 'https://api.moesif.net';
-    public static $VERSION = 'v1.0.0';
+    public static $VERSION = 'v1.1.0';
 
     /**
      * Your Application Id for authentication/authorization
